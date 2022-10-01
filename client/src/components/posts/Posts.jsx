e4a6c712e72bd39ecef8cfc5f306d7903d418ee7
Ghost1706 <56081021+Ghost1706@users.noreply.github.com>
2022-10-01 16:53:15 +0100

    reducer implemented

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Post from "./post/Post";
+import { useSelector } from "react-redux";
 import "./posts.css";
 const Posts = () => {
+  const posts = useSelector((state) => state.posts);
+  console.log(posts);
   return (
     <div className="Posts">
       <div className="post-wrapper">
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Form from "../components/Form/Form";
 import NavBar from "../components/navBar/NavBar";
 import Posts from "../components/posts/Posts";
 import "../index.css";
-
+import { useDispatch } from "react-redux";
+import { getPosts } from "../actions/posts";
 const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
   return (
     <>
       <NavBar />
